6cbdb909a2c8607f079e1cc166e054bf98483382
Suvarnkar Prabhu <41757793+spsuvarnakar@users.noreply.github.com>
2022-09-25 13:56:59 +0530

    Update LineComparisonCompute.java

--- LineComparisonCompute.java
+++ LineComparisonCompute.java
@@ -5,5 +5,28 @@ public class LineComparisonCompute {
 	{
 
 		System.out.println("Welcome to Line Computation Comprison Program ");
+		Scanner length = new Scanner (System.in);
+		System.out.println ("Enter the 1st co-orinate");
+		
+		double x1 = length.nextDouble ();
+		System.out.println ("Enter the 2nd co-orinate");
+		
+		double x2 = length.nextDouble ();
+		System.out.println ("Enter the 3rd co-orinate");
+		
+		double y1 = length.nextDouble ();
+		System.out.println ("Enter the 4th co-orinate");
+		
+		double y2 = length.nextDouble ();
+		
+		double a = (x2 - x1) * (x2 - x1);
+		double b = (y2 - y1) * (y2 - y1);
+		
+		double length_of_line = Math.sqrt (a + b);
+		
+		System.out.println ("Length Of Line : " +length_of_line);  
+		
+		System.out.println("THIS IS LINE(LENGTH)COMPARISON PROGRAM !!!! THANK YOU!! ");
+	
 	}
 }
