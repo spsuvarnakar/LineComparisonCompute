c7092e3e436f9d13a85219aeba5467e6dd17c08d
spsuvarnakar <spsuvarnakar@gmail.com>
2022-09-25 13:19:55 +0530

    Added : Welcome Message

---
+++ LineComparisonCompute.java
@@ -0,0 +1,9 @@
+package com.bridhelabz.LineCoparison;
+
+public class LineComparisonCompute {
+	public static void main(String[] args)
+	{
+
+		System.out.println("Welcome to Line Computation Comprison Program ");
+	}
+}
